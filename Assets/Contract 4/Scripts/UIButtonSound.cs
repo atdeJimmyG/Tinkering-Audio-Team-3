b3245857330499b34c538fc6b3923fcebd7fa1e6
Thomas O'Leary <TO231922@falmouth.ac.uk>
2019-11-05 21:09:59 +0000

    UI now plays click.wav when a mouse hovers/clicks a button

---
+++ Assets/Contract 4/Scripts/UIButtonSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonSound : MonoBehaviour{
+
+    public AudioSource sound;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+
+    public void HoverSound()
+    {
+        sound.PlayOneShot(hoverSound);
+    }
+    public void ClickSound()
+    {
+        sound.PlayOneShot(clickSound);
+    }
+
+
+}
