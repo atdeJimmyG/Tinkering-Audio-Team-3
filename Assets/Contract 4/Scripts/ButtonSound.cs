f162d9ec2dd10e1b9a9083b93dcb4503ad755941
Thomas O'Leary <TO231922@falmouth.ac.uk>
2019-11-25 20:42:50 +0000

    Within ButtonSound.cs there is now a function called ButtonPress that plays a pure tone when called

--- Assets/Contract 4/Scripts/ButtonSound.cs
+++ Assets/Contract 4/Scripts/ButtonSound.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ButtonSound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int pos;
+
+    public int sampleRate = 44100;
+    public int frequency = 440;
+
+    public float sampleDur = 0.5f;
+
+    public string nameOfSample;
+
+    public void ButtonPress()
     {
-        
+        AudioClip audioOutput = AudioClip.Create(nameOfSample, (int)(sampleRate * sampleDur), 1, sampleRate, true, onAudioRead, SetPosition);
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = audioOutput;
+        audio.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    void onAudioRead(float[] samples)
     {
-        
+        int count = 0;
+        while (count < samples.Length)
+        {
+            samples[count] = Mathf.Sin(2 * Mathf.PI * frequency * pos / sampleRate);
+            pos++;
+            count++;
+        }
     }
+
+    void SetPosition(int newPos)
+    {
+        pos = newPos;
+    }
+
 }
