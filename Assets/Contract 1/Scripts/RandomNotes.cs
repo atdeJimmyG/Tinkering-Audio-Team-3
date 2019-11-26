0132c9fc08d227c65083a3251bb68a77ad1ee200
atdeJimmyG <jamesgilljames9@gmail.com>
2019-11-26 00:56:57 +0000

    Added frequencies function in new RandomNotes script

---
+++ Assets/Contract 1/Scripts/RandomNotes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNotes : MonoBehaviour
+{
+    public float[] frequencies;
+
+    void Start()
+    {
+        frequencies = new float[7];
+        frequencies[0] = 261.6f;
+        frequencies[1] = 293.7f;
+        frequencies[2] = 329.6f;
+        frequencies[3] = 349.2f;
+        frequencies[4] = 392.0f;
+        frequencies[5] = 440.0f;
+        frequencies[6] = 493.9f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
